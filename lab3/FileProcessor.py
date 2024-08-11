4a68c5c02ce648cd05325fbb62efa75e87f06c05
Philip Deaton <philipdeaton826@gmail.com>
2024-08-11 14:01:30 -0400

    Huffman tree creation

--- lab3/FileProcessor.py
+++ lab3/FileProcessor.py
@@ -1,6 +1,7 @@
 from typing import TextIO
 from lab3.FrequencyTable import FrequencyTableQueue
 
+
 def process_frequency_table(table_input: TextIO):
     frequency_list = []
 
@@ -23,6 +24,3 @@ def process_frequency_table(table_input: TextIO):
             frequency_list.append((temp_letter, int(temp_freq)))
 
     huffman_queue = FrequencyTableQueue(frequency_list)
-
-
-
--- lab3/FrequencyTable.py
+++ lab3/FrequencyTable.py
@@ -18,5 +18,19 @@ class FrequencyTableQueue:
             self.frequency_queue.append(new_node)
         self.sort_queue()
 
+        while len(self.frequency_queue) > 1:
+
+            right = self.frequency_queue.pop()
+            left = self.frequency_queue.pop()
+
+            print("\nMerging " + left.name + " and " + right.name)
+
+            name = left.name + right.name
+            frequency = left.frequency + right.frequency
+
+            merged_node = Node(name, frequency, left, right)
+            self.frequency_queue.append(merged_node)
+            self.sort_queue()
+
     def sort_queue(self):
         self.frequency_queue.sort(key=comparison_key)

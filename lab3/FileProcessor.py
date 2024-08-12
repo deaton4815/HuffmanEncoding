eb8a5c72f9305781329ee886acbe62ec3adf5229
Philip Deaton <philipdeaton826@gmail.com>
2024-08-11 23:25:56 -0400

    Encoding

--- lab3/FileProcessor.py
+++ lab3/FileProcessor.py
@@ -1,26 +1,45 @@
 from typing import TextIO
 from lab3.FrequencyTable import FrequencyTableQueue
+class FileProcessor:
+
+    def __init__(self, table_input: TextIO, clear_text_input: TextIO):
+        self.huffman_queue = None
+        self.process_frequency_table(table_input)
+
+    def process_frequency_table(self, table_input):
+        frequency_list = []
+
+        while True:
+            line = table_input.readline()
+            if not line:
+                break
+            line = line.strip()
+
+            temp_letter = str()
+            temp_freq = str()
+            for char in line:
+
+                if char.isalpha():
+                    temp_letter = char
+                elif char.isdigit():
+                    temp_freq += char
+
+            if temp_letter and temp_freq:
+                frequency_list.append((temp_letter, int(temp_freq)))
+
+        self.huffman_queue = FrequencyTableQueue(frequency_list)
+
+    def process_clear_text(self, clear_text_input):
+
+        clear_text = []
+
+        while True:
+            line = clear_text_input.readline()
+            if not line:
+                break
+            clear_text.append()
 
 
-def process_frequency_table(table_input: TextIO):
-    frequency_list = []
 
-    while True:
-        line = table_input.readline()
-        if not line:
-            break
-        line = line.strip()
 
-        temp_letter = str()
-        temp_freq = str()
-        for char in line:
 
-            if char.isalpha():
-                temp_letter = char
-            elif char.isdigit():
-                temp_freq += char
-
-        if temp_letter and temp_freq:
-            frequency_list.append((temp_letter, int(temp_freq)))
-
-    huffman_queue = FrequencyTableQueue(frequency_list)
--- lab3/FrequencyTable.py
+++ lab3/FrequencyTable.py
@@ -9,28 +9,49 @@ class FrequencyTableQueue:
 
     def __init__(self, frequency_list):
         self.frequency_queue = []
+        self.preorder_traversal = []
         self.create_huffman_tree(frequency_list)
 
     def create_huffman_tree(self, frequency_list):
+
+        self.initialize_tree(frequency_list)
+
+        while len(self.frequency_queue) > 1:
+            self.merge()
+
+        self.traverse_tree(self.frequency_queue[0])
+
+    def initialize_tree(self, frequency_list):
         # initialize tree
         for entry in frequency_list:
             new_node = Node(entry[0], entry[1], None, None)
             self.frequency_queue.append(new_node)
         self.sort_queue()
 
-        while len(self.frequency_queue) > 1:
+    def merge(self):
+        right = self.frequency_queue.pop()
+        left = self.frequency_queue.pop()
 
-            right = self.frequency_queue.pop()
-            left = self.frequency_queue.pop()
+        name = left.name + right.name
+        frequency = left.frequency + right.frequency
 
-            print("\nMerging " + left.name + " and " + right.name)
-
-            name = left.name + right.name
-            frequency = left.frequency + right.frequency
-
-            merged_node = Node(name, frequency, left, right)
-            self.frequency_queue.append(merged_node)
-            self.sort_queue()
+        merged_node = Node(name, frequency, left, right)
+        self.frequency_queue.append(merged_node)
+        self.sort_queue()
 
     def sort_queue(self):
         self.frequency_queue.sort(key=comparison_key)
+
+    def traverse_tree(self, node: Node) -> None:
+        self.preorder_traversal.append((node.name, node.frequency))
+
+        if node.left:
+            self.traverse_tree(node.left)
+        if node.right:
+            self.traverse_tree(node.right)
+
+    def get_huffman_tree(self):
+        return self.frequency_queue
+
+    def get_preorder_traversal(self) -> str:
+        return self.preorder_traversal

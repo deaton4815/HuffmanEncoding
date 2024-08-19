5bceee0ca0c9468aa24eddbe7e83237c5228676b
Philip Deaton <philipdeaton826@gmail.com>
2024-08-18 21:09:31 -0400

    Lab3 final

--- lab3/Node.py
+++
@@ -1,9 +0,0 @@
-
-class Node:
-
-    def __init__(self, name: str, frequency: int, left: None, right: None):
-
-        self.name = name
-        self.frequency = frequency
-        self.left = left
-        self.right = right
